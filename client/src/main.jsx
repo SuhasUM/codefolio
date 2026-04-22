fe79b063f27d9c86cab04dc9bf43aac1468194b7
Dell <dell@example.com>
2026-04-22 10:16:57 +0530

    fix: redirect apex domain to www before app bootstrap

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './eventzee.css'
 
+if (window.location.hostname === 'eventzee.in') {
+  const targetUrl = `https://www.eventzee.in${window.location.pathname}${window.location.search}${window.location.hash}`;
+  window.location.replace(targetUrl);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
